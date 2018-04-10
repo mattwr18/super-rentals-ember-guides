b717b9f54b0f7625087d654a1b4f43098864394c
Matt Rider <mattwr18@gmail.com>
2018-04-10 09:04:52 -0300

    Initial commit super rentals ember guides

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,42 @@
+import { module, test } from 'qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { visit, currentURL, click, fillIn, triggerKeyEvent } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('should show rentals as the homepage', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+
+  test('should link to about page', async function(assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information', async function(assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+
+  test('should list available rentals', async function(assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3);
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.ok(this.element.querySelector('.results .listing'), 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
+  });
+
+  test('should show details for a selected rental', async function(assert) {
+  });
+});
